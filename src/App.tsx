b99c893db9df7e0860ce584ea37cd0436909239b
Lucas Wamser Rosa Debeterco <lucasdebeterco@gmail.com>
2024-03-26 23:02:17 -0300

    base styling config

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,9 @@
+import { Button } from '@/components/ui/button.tsx'
 
 export function App() {
     return (
-        <></>
+        <div className="flex h-screen items-center justify-center">
+            <Button>Enviar</Button>
+        </div>
     )
 }
