c05bdbf18115bb15e255f3cb7f5f39bf45a63f24
Lucas Wamser Rosa Debeterco <lucasdebeterco@gmail.com>
2024-04-04 00:04:25 -0300

    creating header/nav components

--- src/pages/_layouts/App.tsx
+++ src/pages/_layouts/App.tsx
@@ -1,11 +1,14 @@
 import { Outlet } from 'react-router-dom'
 
+import { Header } from '@/components/Header.tsx'
+
 export function AppLayout() {
     return (
-        <div>
-            <h1>aaa</h1>
-
-            <Outlet />
+        <div className="flex min-h-screen flex-col antialiased">
+            <Header />
+            <div className="flex flex-1 flex-col gap-4 p-8 pt-6">
+                <Outlet />
+            </div>
         </div>
     )
 }
