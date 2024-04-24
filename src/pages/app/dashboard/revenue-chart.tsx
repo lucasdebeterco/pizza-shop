3dd9ffc5770e106157a7aa0df3397841fbed5227
Lucas Wamser Rosa Debeterco <lucasdebeterco@gmail.com>
2024-04-23 23:56:29 -0300

    revenue chart

---
+++ src/pages/app/dashboard/revenue-chart.tsx
@@ -0,0 +1,65 @@
+import {
+    CartesianGrid,
+    Line,
+    LineChart,
+    ResponsiveContainer,
+    XAxis,
+    YAxis
+} from 'recharts'
+import colors from 'tailwindcss/colors'
+
+const data = [
+    {date: '10/12', revenue: 1200},
+    {date: '11/12', revenue: 200},
+    {date: '12/12', revenue: 900},
+    {date: '13/12', revenue: 450},
+    {date: '14/12', revenue: 100},
+    {date: '15/12', revenue: 1100},
+    {date: '16/12', revenue: 300}
+]
+
+import { Card, CardContent, CardDescription, CardHeader, CardTitle } from '@/components/ui/card.tsx'
+export function RevenueChart() {
+    return (
+        <Card className="col-span-6">
+            <CardHeader className="flex-row items-center justify-between pb-8">
+                <div className="space-y-1">
+                    <CardTitle className="text-base font-medium">
+                        Receita do período
+                    </CardTitle>
+                    <CardDescription>
+                        Receita diária no período
+                    </CardDescription>
+                </div>
+            </CardHeader>
+
+            <CardContent>
+                <ResponsiveContainer width="100%" height={240}>
+                    <LineChart data={data} style={{fontSize: 12}}>
+                        <XAxis dataKey="date" axisLine={false} tickLine={false} dy={16} />
+                        <YAxis
+                            stroke="#888"
+                            axisLine={false}
+                            tickLine={false}
+                            tickFormatter={(value: number) =>
+                                value.toLocaleString('pt-BR', {
+                                    style: 'currency',
+                                    currency: 'BRL'
+                                })}
+                            width={80}
+                        />
+
+                        <CartesianGrid vertical={false} className="stroke-muted" />
+
+                        <Line
+                            type="linear"
+                            strokeWidth={2}
+                            dataKey="revenue"
+                            stroke={colors.violet['500']}
+                        />
+                    </LineChart>
+                </ResponsiveContainer>
+            </CardContent>
+        </Card>
+    )
+}
