3a67fa78333571485fe827c1424a8a8e0dce8c5a
Lucas Wamser Rosa Debeterco <lucasdebeterco@gmail.com>
2024-04-28 00:43:17 -0300

    sign in api call

--- src/pages/auth/sign-in.tsx
+++ src/pages/auth/sign-in.tsx
@@ -1,9 +1,11 @@
+import { useMutation } from '@tanstack/react-query'
 import { Helmet } from 'react-helmet-async'
 import { useForm } from 'react-hook-form'
 import { Link } from 'react-router-dom'
 import { toast } from 'sonner'
 import { z } from 'zod'
 
+import { signIn } from '@/api/sign-in.ts'
 import { Button } from '@/components/ui/button.tsx'
 import { Input } from '@/components/ui/input.tsx'
 import { Label } from '@/components/ui/label.tsx'
@@ -19,16 +21,23 @@ export function SignIn() {
         isSubmitting
     }} = useForm<SignInForm>()
 
-    async function handleSignIn(data: SignInForm) {
-        console.log(data)
-        await new Promise(resolve => setTimeout(resolve, 2000))
+    const { mutateAsync: authenticate } = useMutation({
+        mutationFn: signIn,
+    })
 
-        toast.success('Enviamos um link de autenticação para seu e-mail.', {
-            action: {
-                label: 'Reenviar',
-                onClick: () => handleSignIn(data)
-            }
-        })
+    async function handleSignIn(data: SignInForm) {
+        try {
+            await authenticate({ email: data.email })
+
+            toast.success('Enviamos um link de autenticação para seu e-mail.', {
+                action: {
+                    label: 'Reenviar',
+                    onClick: () => handleSignIn(data)
+                }
+            })
+        } catch {
+            toast.error('Credenciais inválidas')
+        }
     }
 
     return (
