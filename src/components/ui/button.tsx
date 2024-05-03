765b2f044b221026d3b68eae7b5129d00b3638a2
Lucas Wamser Rosa Debeterco <lucasdebeterco@gmail.com>
2024-05-02 23:30:05 -0300

    getting data to update profile modal

--- src/components/ui/button.tsx
+++ src/components/ui/button.tsx
@@ -11,7 +11,9 @@ const buttonVariants = cva(
             variant: {
                 default: 'bg-zinc-900 text-zinc-50 hover:bg-zinc-900/90 dark:bg-zinc-50 dark:text-zinc-900 dark:hover:bg-zinc-50/90',
                 destructive:
-          'bg-red-500 text-zinc-50 hover:bg-red-500/90 dark:bg-red-900 dark:text-zinc-50 dark:hover:bg-red-900/90',
+                    'bg-red-500 text-zinc-50 hover:bg-red-500/90 dark:bg-red-900 dark:text-zinc-50 dark:hover:bg-red-900/90',
+                success:
+                    'dark:hover:emerald-600/90 bg-emerald-500 text-white hover:bg-emerald-500/90 dark:bg-emerald-600',
                 outline:
           'border border-zinc-200 bg-white hover:bg-zinc-100 hover:text-zinc-900 dark:border-zinc-800 dark:bg-zinc-950 dark:hover:bg-zinc-800 dark:hover:text-zinc-50',
                 secondary:
