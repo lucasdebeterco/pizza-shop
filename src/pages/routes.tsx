667e965b20ae14eba5611188556c43f7109b8228
Lucas Wamser Rosa Debeterco <lucasdebeterco@gmail.com>
2024-05-19 14:44:18 -0300

    error page route

--- src/pages/routes.tsx
+++ src/pages/routes.tsx
@@ -7,26 +7,32 @@ import { Dashboard } from '@/pages/app/dashboard/dashboard.tsx'
 import { Orders } from '@/pages/app/orders/orders.tsx'
 import { SignIn } from '@/pages/auth/sign-in.tsx'
 import { SignUp } from '@/pages/auth/sign-up.tsx'
+import { Error } from '@/pages/error.tsx'
 
-export const router = createBrowserRouter([{
-    path: '/',
-    element: <AppLayout/>,
-    errorElement: <NotFound />,
-    children: [{
+export const router = createBrowserRouter([
+    {
         path: '/',
-        element: <Dashboard />
+        element: <AppLayout/>,
+        errorElement: <Error />,
+        children: [{
+            path: '/',
+            element: <Dashboard />
+        }, {
+            path: '/orders',
+            element: <Orders />
+        }]
     }, {
-        path: '/orders',
-        element: <Orders />
-    }]
-}, {
-    path: '/',
-    element: <AuthLayout />,
-    children: [{
-        path: '/sign-in',
-        element: <SignIn />
-    },{
-        path: '/sign-up',
-        element: <SignUp />
-    }]
-}])
+        path: '/',
+        element: <AuthLayout />,
+        children: [{
+            path: '/sign-in',
+            element: <SignIn />
+        },{
+            path: '/sign-up',
+            element: <SignUp />
+        }]
+    }, {
+        path: '*',
+        element: <NotFound />
+    }
+])
