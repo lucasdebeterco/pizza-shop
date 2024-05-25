299be4cfc1c84380dad55e5ba633684a7d959466
Lucas Wamser Rosa Debeterco <lucasdebeterco@gmail.com>
2024-05-25 20:53:48 -0300

    creating sign-in api mock

---
+++ src/api/mocks/index.ts
@@ -0,0 +1,13 @@
+import { setupWorker } from 'msw/browser'
+
+import { signInMock } from '@/api/mocks/sign-in-mock.ts'
+import { env } from '@/env.ts'
+
+export const worker = setupWorker(signInMock)
+
+export async function enableMSW() {
+    if(env.MODE !== 'test') {
+        return
+    }
+    await worker.start()
+}
--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,14 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 
+import { enableMSW } from '@/api/mocks'
+
 import { App } from './App.tsx'
 
-ReactDOM.createRoot(document.getElementById('root')!).render(
-    <React.StrictMode>
-        <App /> 
-    </React.StrictMode>,
-)
+enableMSW().then(() => {
+    ReactDOM.createRoot(document.getElementById('root')!).render(
+        <React.StrictMode>
+            <App />
+        </React.StrictMode>,
+    )
+})
