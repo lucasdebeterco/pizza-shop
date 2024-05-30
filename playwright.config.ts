551998edde58d8b8c88b224e87b074aeeb3ee0c7
Lucas Wamser Rosa Debeterco <lucasdebeterco@gmail.com>
2024-05-30 17:20:51 -0300

    test github actions

--- playwright.config.ts
+++ playwright.config.ts
@@ -7,7 +7,7 @@ export default defineConfig({
     forbidOnly: !!process.env.CI,
     retries: process.env.CI ? 2 : 0,
     workers: process.env.CI ? 1 : undefined,
-    // reporter: 'html',
+    reporter: 'html',
     use: {
         baseURL: 'http://localhost:5000',
     },
